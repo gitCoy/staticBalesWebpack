641a8e7aa6236a0c57c6265db30e5339c625ceb9
823100926@qq.com <823100926@qq.com>
2018-11-30 19:46:22 +0800

    。。。

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
+const pathCom = '/assets/dev'
 export default {
-  input: './assets/dev/javascript/application.js',
+  input: [
+    __dirname + pathCom + '/javascript/application.js',
+    __dirname + pathCom + '/vendor/jquery-3.1.0.min.js',
+    __dirname + pathCom + '/vendor/scrollify.js',
+    __dirname + pathCom + '/vendor/slick.min.js'
+  ],
   output: {
     file: './dist/main.min.js',
     format: 'umd'
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,69 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const pathCom = './assets/dev'
+const pathCom = '/assets/dev'
 module.exports = {
-  entry: __dirname + '/index.js',
+  entry: {
+    index: __dirname + pathCom + '/javascript/application.js',
+    vendor: [
+      __dirname + pathCom + '/vendor/jquery-3.1.0.min.js',
+      __dirname + pathCom + '/vendor/scrollify.js',
+      __dirname + pathCom + '/vendor/slick.min.js'
+    ]
+  },
   output: {
     path: __dirname + '/public',
-    filename: 'test.js'
+    filename: '[name].min.js?[hash]'
+  },
+
+  plugins: [
+    new CleanWebpackPlugin('public'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './' + pathCom + '/html/page/index.pug',
+      hash: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'company.html',
+      template: './' + pathCom + '/html/page/company.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'commercial.html',
+      template: './' + pathCom + '/html/page/commercial.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'philosophy.html',
+      template: './' + pathCom + '/html/page/philosophy.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'privacyPolicy.html',
+      template: './' + pathCom + '/html/page/privacyPolicy.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'product1.html',
+      template: './' + pathCom + '/html/page/product1.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'product2.html',
+      template: './' + pathCom + '/html/page/product2.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'shopList.html',
+      template: './' + pathCom + '/html/page/shopList.pug'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: '/\.pug$/',
+        use: [
+          'raw-loader',
+          'pug-html-loader'
+        ]
+      }
+    ]
   },
   devtool: 'eval-source-map',
   devServer: {
